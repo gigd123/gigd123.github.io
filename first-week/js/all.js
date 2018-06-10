8bfb6a100b4f816feb357cf1478affb348fcca91
奉一 <gigdleo@gmail.com>
2018-06-11 00:03:20 +0800

    first-week3

--- first-week/js/all.js
+++ first-week/js/all.js
@@ -3,7 +3,7 @@ const addTask = document.getElementById('addTask');
 addTask.addEventListener('focus',addTitleBox);
 const taskList = document.querySelector('.taskList');
 const addBox = document.querySelector('.addBox');
-const title = document.querySelector('.title');
+const title = document.querySelector('.addTitle');
 const data = JSON.parse(localStorage.getItem('allData')) || [];
 
 /*focus在addTask時候，隱藏addBox，顯示title編輯畫面*/ 
@@ -69,40 +69,56 @@ function updateList(items){
                 <i class="far fa-comment-dots fa-xs"></i>
             </span>
         </div>
-        <ul class="editTask">
-            <li class="deadLine">
-                <span>
-                    <i class="far fa-calendar-alt fa-xs"></i>Deadline
-                </span>
-                <h2>
-                    <input type="date" name="" id="deadLine-date" value="${items[i].deadLineDate}">
-                    <input type="time" name="" id="deadLine-time" value="${items[i].deadLineTime}">
-                </h2>
-            </li>
-            <li>
-                <span>
-                    <i class="far fa-file fa-xs"></i>File
-                </span>
-                <h2>
-                    <label for="task-file" class="task-file">
-                        <input id="task-file" type="file" name="" id="" hidden="hidden">
-                    </label>
-                </h2>
-            </li>
-            <li>
-                <span>
-                    <i class="far fa-comment-dots fa-xs"></i>Comment
-                </span>
-                <h2>
-                    <textarea id="title-comment" name="" id="" cols="30" rows="10" placeholder="Type your memo here…">${items[i].comment}</textarea>
-                </h2>
-            </li>
-            <li class="editTask-btn">
-                <a href="#" class="editTask-btn-cancel">× Cancel</a>
-                <a href="#" class="editTask-btn-add">+ Add Task</a>
-            </li>
-        </ul>
+        <div class="editTask-hide">
+            <ul class="editTask">
+                <li class="deadLine">
+                    <span>
+                        <i class="far fa-calendar-alt fa-xs"></i>Deadline
+                    </span>
+                    <h2>
+                        <input type="date" name="" id="deadLine-date" value="${items[i].deadLineDate}">
+                        <input type="time" name="" id="deadLine-time" value="${items[i].deadLineTime}">
+                    </h2>
+                </li>
+                <li>
+                    <span>
+                        <i class="far fa-file fa-xs"></i>File
+                    </span>
+                    <h2>
+                        <label for="task-file" class="task-file">
+                            <input id="task-file" type="file" name="" id="" hidden="hidden">
+                        </label>
+                    </h2>
+                </li>
+                <li>
+                    <span>
+                        <i class="far fa-comment-dots fa-xs"></i>Comment
+                    </span>
+                    <h2>
+                        <textarea id="title-comment" name="" id="" cols="30" rows="10" placeholder="Type your memo here…">${items[i].comment}</textarea>
+                    </h2>
+                </li>
+                <li class="editTask-btn">
+                    <a href="#" class="editTask-btn-cancel">× Cancel</a>
+                    <a href="#" class="editTask-btn-add">+ Add Task</a>
+                </li>
+            </ul>
+        </div>
     </li>`;
     }
     taskList.innerHTML =str;
-}
+};
+/*點擊pencil，編輯task*/ 
+const editTask = document.querySelector('.new-title .title-roof .title-edit');
+const showlEdit = document.querySelector('.new-title .editTask-hide');
+editTask.addEventListener('click',editTaskData);
+function editTaskData(){
+    showlEdit.classList.add('active');
+};
+/*點擊cancel時候，關閉addTask，顯示addBox*/ 
+const cancelEdit = document.querySelector('.new-title .editTask-hide .editTask .editTask-btn .editTask-btn-cancel');
+cancelEdit.addEventListener('click',cancelAdd);
+function cancelAdd(e){
+    e.preventDefault();
+    showlEdit.classList.remove('active');
+};
